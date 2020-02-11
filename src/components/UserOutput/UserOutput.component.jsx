b66057462090b428a561866ac3748fdf353cca56
banobepascal <banobepascal@gmail.com>
2020-02-11 20:15:33 +0300

    ft(syntax exercise):
    - added userInput component
    - added userOutput component
    - applied state and manipulation
    
    Finishes

---
+++ src/components/UserOutput/UserOutput.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserOutput = props => {
+  return (
+    <div>
+      <p>Username: {props.userName}</p>
+      <p>Some nice text</p>
+    </div>
+  );
+};
+
+export default UserOutput;

9aca08fe1bae2a0c4d2431f7c3081be87dffe0ea
banobepascal <banobepascal@gmail.com>
2020-02-18 18:31:57 +0300

    ch(fix errors): fixing app errors

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -17,7 +17,7 @@ const cockpit = props => {
 
   return (
     <div className={classes.Cockpit}>
-      <h1>You are learning react</h1>
+      <h1>{props.title}</h1>
       <p className={assignedClasses}>Results are available</p>
       <p className={showExp}>Sorry no results</p>
       <button className={btnClass} onClick={props.clicked}>
--- src/containers/App.js
+++ src/containers/App.js
@@ -59,6 +59,7 @@ class App extends Component {
     return (
       <div className={classes.App}>
         <Cockpit
+          title={this.props.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           clicked={this.togglePersonsHandler}
